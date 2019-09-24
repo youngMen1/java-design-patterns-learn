28a93a6a386edfc69a21669c751c1629fc97ad9b
youngMen1 <fengzhiqiangzxx199644>
2019-09-24 15:13:07 +0800

    DESC:配置信息

--- factory-method/src/main/java/com/seal/factory/FactoryApplication.java
+++ factory-method/src/main/java/com/seal/factory/FactoryApplication.java
@@ -2,7 +2,12 @@ package com.seal.factory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/9/11 17:53
+ * @description 工厂方法模式
+ **/
 @SpringBootApplication
 public class FactoryApplication {
 
