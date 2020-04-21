e8db7f2e1f7b359b01de6152bc414027829deb21
youngMen1 <fengzhiqiangzxx199644>
2020-04-21 14:31:22 +0800

    DESC:整理

--- strategy/src/main/java/com/seal/strategy/service/Strategy.java
+++ strategy/src/main/java/com/seal/strategy/service/Strategy.java
@@ -4,8 +4,14 @@ package com.seal.strategy.service;
  * @author zhiqiang.feng
  * @version 1.0
  * @date-time 2019/9/26 11:11
- * @description
+ * @description 定义一个策略接口
  **/
 public interface Strategy {
+    /**
+     * 定义一个策略接口
+     * @param num1
+     * @param num2
+     * @return
+     */
     int doOperation(int num1, int num2);
 }
